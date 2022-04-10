732d14bba2e283a7b595dee2b6918dfa098df692
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-04-10 13:20:47 -0400

    Added github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This app was coded by Rishona Lee and is {""}
+        <a
+          href="https://github.com/Rishona9/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-source
+        </a>
+      </footer>
     </div>
   );
 }
