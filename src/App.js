421ca9d488551b3e5df85b8b623cf78dbb1ce509
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-04-10 13:08:49 -0400

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <Search />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,1 @@
 import React from "react";
-import "./Weather.css";
-
-export default function Weather(props) {
-  return (
-    <ul className="showWeather">
-      <li>Temperature: {Math.round(props.temperature)}°F</li>
-      <li>Description: {props.description} </li>
-      <li>Humidity: {props.humidity}% </li>
-      <li>Wind: {Math.round(props.wind)}mph </li>
-      <li>
-        <img
-          alt=""
-          src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
-        />
-      </li>
-    </ul>
-  );
-}
